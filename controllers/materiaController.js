591a8e095da477d18f932534185573702b562a21
André Nas <andrenascontato@gmail.com>
2021-06-14 20:19:23 -0300

    corrigindo o controller da aplicação ao retornar lista de materias

--- controllers/materiaController.js
+++ controllers/materiaController.js
@@ -17,23 +17,51 @@ const addMateria = async(req, res, next) =>{
 
 const getAllMaterias = async (req, res, next) =>{
     try{
+
+        let {page, limit= 2 } = req.query;
+
+        if(!page){
+            page = 1;
+        }
+
+        /* Para paginar a api de n em n materias preciso ter a referencia do ultimo item entao: 
+        let last = data.docs[snapshot.docs.length - 1];
+        let first = data.docs[snapshot.docs[0]]
+
+        startAt(A) = [ A - Z]
+        startAfter(A) = [B - Z]
+        endAt(J) = [A - J]
+        endBefore(J) = [A - I]
+        
+        //next page
+        const nextt = firestore.collection('materia').startAfter(last.data()).limit(3);
+        //back page
+        const prev = firestore.collection('materia').endBefore(first.data()).limit(3);
+        
+        */
+        
+
         const materias = await firestore.collection('materia');
         const data = await materias.get();
+
+        const position = data.docs[page * 1];
+
         const materiaArray = [];
+
         if(data.empty){
             res.status(404).send('Ainda não há publicações')
         }else{
             data.forEach(doc =>{
-                const materia = new Materia(
-                    doc.id,
-                    doc.data().title,
-                    doc.data().description,
-                    doc.data().image,
-                    doc.data().published
-                );
+                const materia = {  
+                    id: doc.id,
+                    title: doc.data().title,
+                    description: doc.data().description,
+                    image: doc.data().image,
+                    published: doc.data().published
+                };
                 materiaArray.push(materia);
             })
-            res.send(materiaArray)
+            res.send({page, data: materiaArray});
         }
     }catch(err){
         res.status(400).send(err.message);
--- models/materia.js
+++ models/materia.js
@@ -1,13 +1,14 @@
 class Materia{
-    constructor(id, title, description, text, image, image_credit, published){
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.text = text;
-        this.image = image;
-        this.image_credit = image_credit;
-        this.published = published;
-    }
-}
+
+ constructor(id, title, description, text, image, image_credit, published){
+     this.id = id;
+     this.title = title;
+     this.description = description;
+     this.text = text;
+     this.image = image;
+     this.image_credit = image_credit;
+     this.published = published;
+ }
+} 
 
 module.exports = Materia;
